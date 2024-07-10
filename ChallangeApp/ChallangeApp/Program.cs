41f899fc9b433f0fea198adb85c81927395ca727
Wojciech Pekala <peki872@wp.pl>
2024-07-10 01:45:11 +0100

    zadanie domowe dzień 4

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, Wojciech!");
+﻿int age = 30;
+string name = "Ewa";
+string sex = "mężczyzna";
+
+
+
+if (sex == "kobieta" && age < 30)
+{
+    Console.WriteLine("kobieta poniżej 30 lat");
+}
+if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+if (sex == "mężczyzna" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
