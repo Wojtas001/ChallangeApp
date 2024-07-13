099d61f0efd476733bc969165cbf962059972dc9
Wojciech Pekala <peki872@wp.pl>
2024-07-13 01:47:25 +0100

    zadanie domowe dzien 5

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,18 +1,62 @@
-﻿int age = 30;
-string name = "Ewa";
-string sex = "mężczyzna";
+﻿int number = 950542738;
+string numberInString = number.ToString();
+List<char> letters = numberInString.ToList();
 
 
-
-if (sex == "kobieta" && age < 30)
+int[] counter = new int[10];
+for (int i = 0; i < counter.Length; i++)
 {
-    Console.WriteLine("kobieta poniżej 30 lat");
+    counter[i] = 0;
 }
-if (name == "Ewa" && age == 30)
+
+
+foreach (char letter in letters)
 {
-    Console.WriteLine("Ewa, lat 30");
+    if (letter == '0')
+    {
+        counter[0]++;
+    }
+    else if (letter == '1')
+    {
+        counter[1]++;
+    }
+    else if (letter == '2')
+    {
+        counter[2]++;
+    }
+    else if (letter == '3')
+    {
+        counter[3]++;
+    }
+    else if (letter == '4')
+    {
+        counter[4]++;
+    }
+    else if (letter == '5')
+    {
+        counter[5]++;
+    }
+    else if (letter == '6')
+    {
+        counter[6]++;
+    }
+    else if (letter == '7')
+    {
+        counter[7]++;
+    }
+    else if (letter == '8')
+    {
+        counter[8]++;
+    }
+    else if (letter == '9')
+    {
+        counter[9]++;
+    }
 }
-if (sex == "mężczyzna" && age < 18)
+
+Console.WriteLine("W liczbie '" + number + "' ilość zawartych cyfr występuje:");
+
+for (int i = 0; i < 10; i++)
 {
-    Console.WriteLine("Niepełnoletni mężczyzna");
+    Console.WriteLine(i + " --> " + counter[i]);
 }
